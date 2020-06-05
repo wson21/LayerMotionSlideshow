239bd9223da8bece88ea17d5b349b12e83ef4bd4
wson21 <wson21@yahoo.com>
2020-06-05 18:26:39 +0800

    update

--- index.php
+++ index.php
@@ -34,6 +34,27 @@
     </section>
 
     <section class="what__we__do">
+        
+        <div class="left">
+
+            <div class="wrap">
+
+            <h1>Everything <br>
+                 You Can Dream of,<br>
+                We Make it a Reality.
+            </h1>
+    
+            <p>At Simfur Design, we are not just designers or builders; we are an all-in-one solution provider. That means that not only do we help you to design your kitchen, wardrobe or even your entire house or office, the way you want it, we are also able to manage your entire project for you, from design consultation all the way to manufacturing and installation, as well as liaise with all the relevant contractors and supploers - so you have an enjoyable experience while making your dream a vivid reality.</p>
+
+            <div class="btn">what we do</div>
+
+            </div>
+        </div>
+        
+        <div class="right">
+            <img src="<?php echo get_template_directory_uri() ?>/images/whatwedo.jpg" class="img-fluid">
+            <div class="img_shadow"></div>
+        </div>
 
     </section>
 
@@ -65,37 +86,84 @@
         
     </section>
 
-    <section class="project">
-        <div></div>
+    <section class="projects">
+        project photo
     </section>
 
     <section class="philosophy">
- 
-            <div class="box">
-                <img src="<?php echo get_template_directory_uri() ?>/images/quality.svg" class="icon_size">
-                <h3>QUALITY</h3>
-                is our first priority.
-            </div>
-
-            <div class="box">
-            <img src="<?php echo get_template_directory_uri() ?>/images/design.svg" class="icon_size">
-                <h3>DESIGN</h3>
-                is based on originality,<br/>
-                aesthetics and functionality.
-            </div>
-
-            <div class="box">
-            <img src="<?php echo get_template_directory_uri() ?>/images/price.svg" class="icon_size">
-                <h3>PRICE</h3>
-                is always competitive.
+    <h3>OUR BUSINESS PHILOSOPHY</h3>
+            <div class="box_wrap">
+                <div class="box">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/quality.svg" class="icon_size">
+                    <h3>QUALITY</h3>
+                    is our first priority.
+                </div>
+    
+                <div class="box">
+                <img src="<?php echo get_template_directory_uri() ?>/images/design.svg" class="icon_size">
+                    <h3>DESIGN</h3>
+                    is based on originality,<br/>
+                    aesthetics and functionality.
+                </div>
+    
+                <div class="box">
+                <img src="<?php echo get_template_directory_uri() ?>/images/price.svg" class="icon_size">
+                    <h3>PRICE</h3>
+                    is always competitive.
+                </div>
             </div>
 
     </section>
 
     <section class="contact">
 
+        <div class="contact_wrap">
+
+        <h3 class="text_center">start a project now</h3>
+
+        <p class="text_center">We always welcome new opportunities, whether large, small or artisanal.</p>
+
+        
+            <div class="container">
+                
+                <div class="left enquiry">enquiry</div>
+                <div class="right info">
+                <h4>​​Simfur Design Sdn Bhd</h4><br><br>
+    
+                <h5>SHOWROOM</h5><br>
+    
+                Unit 6-6, The CEO, Lebuh Nipah 5,<br>
+    
+                Bayan Lepas 11900 Penang.<br><br>
+    
+                T  604 - 637 1177<br><br>
+    
+                <h5>PRODUCTION OFFICE</h5><br>
+    
+                309-G MK 12, Sungai Tiram,<br>
+    
+                Bayan Lepas 11900 Penang.<br><br>
+    
+                T 604 - 643 4069<br>
+                T 604 - 641 3188<br>
+                F 604 - 643 5051<br>
+                M 6017 - 467 9333<br><br>
+    
+                E sales@simfur.com<br><br>
+    
+                <h5>Follow US</h5><br>
+                    <ul>
+                        <li>facebook</li>
+                        <li>instagram</li>
+                        <li>pinterest</li>
+                    </ul>
+    
+                </div>
+            </div>
+        </div>
+
     </section>
 
-</di>
+</div>
 
 <?php include('footer.php'); ?>
